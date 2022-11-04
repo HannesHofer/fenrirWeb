232eb409f9c4807f7e28f4744e9b0884b2490b3b
Hannes Hofer <Hannes.Hofer@gmail.com>
2022-11-04 19:02:14 +0100

    add async call to store config and get result

--- static/script.js
+++ static/script.js
@@ -10,12 +10,12 @@ async function getData(url = '',  data) {
 		referrerPolicy: 'no-referrer',
 		body: JSON.stringify(data)
 	});
-	return response.json();
+	return response;
 }
 
 function changeState(id)
 {
-	getData("/changestate", {ip: id, state: 'toggle'}).then(resp => {
+	getData("/changestate", {ip: id, state: 'toggle'}).then(resp => resp.json()).then(resp => {
 		var row = document.getElementById('img-' + resp['ip']);
 		row.src = 'static/' + resp['state'] + '.png'
 	})
@@ -27,4 +27,12 @@ function replaceHREFS()
 	Array.from(items).forEach((element) => {
 		element.href = 'javascript:void(0);'
 	});
+}
+
+async function sendConfig(data, ) {
+	data = {vpnconfig: document.getElementById("vpnconfig").value,
+	        vpnauth: document.getElementById("vpnauth").value}
+	getData("/storeconfig", data).then(resp => resp.text()).then(resp => {
+		document.getElementById("header").innerHTML += resp
+	})
 }
