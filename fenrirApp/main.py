adcf43f1cd3d84da8efcf4ce48c6804d556886dd
Hannes Hofer <Hannes.Hofer@gmail.com>
2023-04-01 23:45:48 +0200

    use views files to render templates

--- fenrirApp/main.py
+++ fenrirApp/main.py
@@ -1,10 +1,10 @@
-from bottle import run, Bottle
+from bottle import Bottle, run
 from logging import basicConfig, INFO, DEBUG
 from sys import stdout
 from argparse import ArgumentParser
-from view_main import app as main_app
-from view_config import app as config_app
-from lib import initDB
+from . view_main import app as main_app
+from . view_config import app as config_app
+from . lib import initDB, set_needspasswordcheck, checkvpnpassword, is_passworusedforencryption
 
 
 def make_app() -> Bottle:
@@ -30,6 +30,7 @@ def main() -> None:
     args = parser.parse_args()
     basicConfig(stream=stdout, level=DEBUG if args.debug else INFO)
     initDB()
+    set_needspasswordcheck(is_passworusedforencryption() and not checkvpnpassword())
     run(app, host='0.0.0.0', port=8080, debug=args.debug, reloader=True)
 
 
