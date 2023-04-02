611be674d00b3341bd32ffaedcd82961048b8ee7
Hannes Hofer <Hannes.Hofer@gmail.com>
2023-04-02 21:07:21 +0200

    add internal version to and long description

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup
+import fenrirWeb
 
 setup(
     name='fenrirWeb',
-    version='0.1.0',
-    long_description=__doc__,
+    version=fenrirWeb.__version__,
+    long_description="""Fenrir provides a user friendly way to route all traffic from configured trough a VPN tunnel.
+
+FenrirWeb is a webinterface for viewing/ configuring fenrir.
+    """,
     authors=['Hannes Hofer <Hannes.Hofer@gmail.com>'],
     description='use FenrirWeb as WebInterface for fenrir in order to show and configure VPN route enforcement',
     packages=['fenrirWeb'],
     include_package_data=True,
     zip_safe=False,
-    install_requires=['Bottle', 'jinja2', 'bcrypt', 'fenrir @ https://github.com/HannesHofer/fenrir/releases/download/0.3.0/fenrir-0.3.0-py3-none-any.whl'],
+    install_requires=['Bottle', 'jinja2', 'bcrypt', 'fenrircore'],
     entry_points={'console_scripts': ['fenrirWeb = fenrirWeb.main:main', ]},
 )
