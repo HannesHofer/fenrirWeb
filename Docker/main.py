ddd50957dec8b1df9857331a982470d119315df7
Hannes Hofer <Hannes.Hofer@gmail.com>
2023-04-10 10:42:33 +0200

    remove content for Github pages branch

--- Docker/main.py
+++
@@ -1,5 +0,0 @@
-#!/bin/env python3
-
-from fenrirWeb.main import app
-from fenrirWeb import static
-from fenrirWeb import views
--- Docker/prestart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p /var/cache/fenrir
-if [ ${INTERFACE} ]; then
-    echo "executing: fenrir --inputinterface ${INTERFACE}"
-    fenrir --inputinterface ${INTERFACE} &
-else
-    fenrir &
-fi
